c5c8b1a9e2a161278709b1033810953a88e9c57f
markoparkermarsenic <markoparkermarsenic@gmail.com>
2022-05-18 18:39:37 +0100

    rename log filepath

--- config_parse.py
+++ config_parse.py
@@ -4,7 +4,7 @@ import configparser
 def get_config(path):
     config = configparser.ConfigParser()
     config["SERVER"] = {"Port": "1999", "Host": "localhost"}
-    config["LOGGING"] = {"Level": "debug", "Path": "/var/log/sumulus/sumulus.log"}
+    config["LOGGING"] = {"Level": "debug", "Path": "/var/log/spout/sprout.log"}
 
     with open(path, "w") as configfile:
         config.write(configfile)
