6b55f7ff409f3d07cc474e3a58e71c954fff543b
markoparkermarsenic <markoparkermarsenic@gmail.com>
2022-05-18 19:03:20 +0100

    typo spout to sprout

--- config_parse.py
+++ config_parse.py
@@ -4,7 +4,7 @@ import configparser
 def get_config(path):
     config = configparser.ConfigParser()
     config["SERVER"] = {"Port": "1999", "Host": "localhost"}
-    config["LOGGING"] = {"Level": "debug", "Path": "/var/log/spout/sprout.log"}
+    config["LOGGING"] = {"Level": "debug", "Path": "/var/log/sprout/sprout.log"}
 
     with open(path, "w") as configfile:
         config.write(configfile)
